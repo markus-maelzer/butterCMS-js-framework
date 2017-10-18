362f311b1c42c3680190e533ace5f0f3039bc88c
Markus Mälzer <markus.maelzer@gmail.com>
2017-10-18 13:46:23 +0200

    structure changes

---
+++ devFiles/posts.js
@@ -0,0 +1,17 @@
+// data needed to fetch
+// author
+// body
+// categories
+// tags
+// published (publish date)
+
+// SEO stuff
+// feature_image
+// meta description ??
+
+butter.post.list({page: 1, page_size: 2}).then(function(response) {
+  console.log(response.data)
+
+
+
+});
--- posts.js
+++
@@ -1,8 +0,0 @@
-
-
-butter.post.list({page: 1, page_size: 2}).then(function(response) {
-  console.log(response.data)
-
-
-
-});
